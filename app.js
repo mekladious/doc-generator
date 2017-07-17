51c6b331a22c2dcc28b93b9161b4fae84d1eaf01
mekladious <miraekladious@gmail.com>
2017-07-17 17:06:40 +0200

    added static folder

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.get('/', (req, res)=>{
 
 app.get('*', (req, res)=>{
     console.log("x");
-    res.sendFile(path.join(__dirname+ '/angular-src/dist/index.html'));
+    res.sendFile(path.join(__dirname+ 'angular-src/dist/index.html'));
 })
 
 //Start Server
