03ca80503921bb9250603fbf0a7ff90ce72c1910
mekladious <miraekladious@gmail.com>
2017-07-18 11:53:09 +0200

    test

--- app.js
+++ app.js
@@ -47,11 +47,7 @@ app.get('/', (req, res)=>{
 });
 
 app.get('*', (req, res)=>{
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-    res.header("Access-Control-Allow-Credentials", "true");
-    res.header("Access-Control-Allow-Headers", "Authorization, Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-    next();
+    res.sendFile(path.join(__dirname, '/angular/dist/index.html'));
 });
 
 //Start Server
